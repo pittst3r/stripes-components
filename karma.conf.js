3b526f21962c9fc225cb7f7bba9ec8a351af0027
carolegodfrey <cgodfrey@ebsco.com>
2019-07-25 08:49:15 -0400

    Add karma-viewport for screen size testing and skipped tests on small screen

--- karma.conf.js
+++ karma.conf.js
@@ -24,12 +24,41 @@ module.exports = (config) => {
         os: 'Windows',
         os_version: '10'
       }
+    },
+    viewport: {
+      breakpoints: [
+        {
+          name: 'mobile',
+          size: {
+            width: 320,
+            height: 480
+          }
+        },
+        {
+          name: 'tablet',
+          size: {
+            width: 768,
+            height: 1024
+          }
+        },
+        {
+          name: 'screen',
+          size: {
+            width: 1440,
+            height: 900
+          }
+        }
+      ]
     }
   };
 
   // BrowserStack launcher isn't automatically added by Stripes CLI
   configuration.plugins = config.plugins;
   configuration.plugins.push('karma-browserstack-launcher');
+  configuration.plugins.push('karma-viewport');
+
+  configuration.frameworks = config.frameworks;
+  configuration.frameworks.push('viewport');
 
   config.set(configuration);
 };
